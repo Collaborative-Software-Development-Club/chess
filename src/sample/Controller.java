0a18e43363deb5c4e826f6d1795147e5883abb2b
Franz Lleshaj <66387518+franzlleshaj@users.noreply.github.com>
2021-09-14 11:40:53 -0400

    Added .gitignore file

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,40 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button singleplayerBtn;
+
+    @FXML
+    private Button multiplayerBtn;
+
+    @FXML
+    private Button settingsBtn;
+
+    @FXML
+    private Button quitBtn;
+
+    @FXML
+    void openSettings(ActionEvent event) {
+
+    }
+
+    @FXML
+    void quitGame(ActionEvent event) {
+
+    }
+
+    @FXML
+    void startMultiplayer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void startSingleplayer(ActionEvent event) {
+
+    }
+
 }
