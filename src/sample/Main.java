a2da96d72e3c064afe448e531b9d29bd684fdeb0
Franz Lleshaj <66387518+franzlleshaj@users.noreply.github.com>
2021-09-14 20:07:31 -0400

    fixed allignment

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Chess");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 500, 400));
         primaryStage.show();
     }
 
