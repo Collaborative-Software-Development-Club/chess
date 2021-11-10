45f32c72d43462339073833df65e918b20456d3c
Franz Lleshaj <66387518+franzlleshaj@users.noreply.github.com>
2021-11-09 19:24:31 -0500

    added chess piece image method to MultiplayerController

--- src/sample/MultiplayerController.java
+++ src/sample/MultiplayerController.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -35,7 +36,7 @@ public class MultiplayerController {
 
     public class chessPiece {
         String name;
-        Boolean isWhiteTeam;
+        Boolean isBlueTeam;
         Boolean isDead;
     }
 
@@ -57,7 +58,6 @@ public class MultiplayerController {
         imageView.setFitWidth(575);
         imageView.setFitHeight(575);
         imageView.setPreserveRatio(false);
-        root = Controller.setRoot(imageView);
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
@@ -77,14 +77,14 @@ public class MultiplayerController {
                 chessBoard[i][j] = new chessSquare();
                 chessBoard[i][j].chessPiece = new chessPiece();
                 chessBoard[i][j].isEmpty = false;
-                chessBoard[i][j].chessPiece.isWhiteTeam = false;
+                chessBoard[i][j].chessPiece.isBlueTeam = false;
                 chessBoard[i][j].chessPiece.isDead = false;
             }
             for (int j = 6; j < 8; j++) {
                 chessBoard[i][j] = new chessSquare();
                 chessBoard[i][j].chessPiece = new chessPiece();
                 chessBoard[i][j].isEmpty = false;
-                chessBoard[i][j].chessPiece.isWhiteTeam = true;
+                chessBoard[i][j].chessPiece.isBlueTeam = true;
                 chessBoard[i][j].chessPiece.isDead = false;
             }
         }
@@ -114,7 +114,9 @@ public class MultiplayerController {
         chessBoard[6][7].chessPiece.name = "horse";
         chessBoard[7][7].chessPiece.name = "rook";
 
-        gameState = "pendingWhite"; // white piece needs to be picked
+        root = setRoot(imageView); // add chess piece images
+
+        gameState = "pickBlue"; // blue piece needs to be picked
 
         startGame(event);
 
@@ -150,8 +152,8 @@ public class MultiplayerController {
     int [] score(chessSquare[][] chessBoard){
 
         int [] score = new int[2];
-        // first element is white score
-        // second element is black score
+        // first element is blue score
+        // second element is purple score
         score[0] = 0;
         score[1] = 1;
 
@@ -159,7 +161,7 @@ public class MultiplayerController {
             for (int j = 0; j < 8; j++){
                 if (!chessBoard[i][j].isEmpty){
                     if (!chessBoard[i][j].chessPiece.isDead){
-                        if (chessBoard[i][j].chessPiece.isWhiteTeam){
+                        if (chessBoard[i][j].chessPiece.isBlueTeam){
                             score[0] ++;
                         }
                         else{
@@ -177,45 +179,45 @@ public class MultiplayerController {
     void makeMove(ActionEvent event, int[] coordinates) throws IOException { // executes with every mouse click
         int xCoord = coordinates[0];
         int yCoord = coordinates[1];
-        if (gameState == "pendingWhite") { // pendingWhite is when you have to pick a white chessPiece
-            pickWhite(xCoord, yCoord);
-        } else if (gameState == "pendingBlack") { // same as above but for black chess piece
-            pickBlack(xCoord, yCoord);
-        } else if (gameState == "whitePick") { // once the white piece has been picked, pick where to put it
-            whitePickSpot(xCoord, yCoord);
-        } else if (gameState == "blackPick") { // same as above
-            blackPickSpot(xCoord, yCoord);
+        if (gameState == "pickBlue") { // pickBlue is when you have to pick a blue chessPiece
+            pickBlue(xCoord, yCoord);
+        } else if (gameState == "pickPurple") { // same as above but for purple chess piece
+            pickPurple(xCoord, yCoord);
+        } else if (gameState == "bluePickSpot") { // once the blue piece has been picked, pick where to put it
+            bluePickSpot(xCoord, yCoord);
+        } else if (gameState == "purplePickSpot") { // same as above
+            purplePickSpot(xCoord, yCoord);
         }
     }
 
     @FXML
-    void pickWhite(int xCoord, int yCoord) {
-        // if a white chess piece is selected, then the state is changed and its coordinates are saved
-        if (!chessBoard[xCoord][yCoord].isEmpty && chessBoard[xCoord][yCoord].chessPiece.isWhiteTeam) {
-            gameState = "whitePick";
+    void pickBlue(int xCoord, int yCoord) {
+        // if a blue chess piece is selected, then the state is changed and its coordinates are saved
+        if (!chessBoard[xCoord][yCoord].isEmpty && chessBoard[xCoord][yCoord].chessPiece.isBlueTeam) {
+            gameState = "bluePickSpot";
             pickedXCoord = xCoord;
             pickedYCoord = yCoord;
         }
     }
 
-    void pickBlack(int xCoord, int yCoord) throws IOException {
+    void pickPurple(int xCoord, int yCoord) throws IOException {
 
-        // is the piece black, if so change game state to blackPick
+        // is the piece purple, if so change game state to purplePickSpot
 
-        if ((!chessBoard[xCoord][yCoord].isEmpty) && (!chessBoard[xCoord][yCoord].chessPiece.isWhiteTeam)){
-                gameState = "blackPick";
+        if ((!chessBoard[xCoord][yCoord].isEmpty) && (!chessBoard[xCoord][yCoord].chessPiece.isBlueTeam)){
+                gameState = "purplePickSpot";
                 pickedXCoord = xCoord;
                 pickedYCoord = yCoord;
         }
     }
 
 
-    void blackPickSpot(int xCoord, int yCoord) throws IOException {
+    void purplePickSpot(int xCoord, int yCoord) throws IOException {
 
         if (chessBoard[pickedXCoord][pickedYCoord].chessPiece.name.equals("pawn")){
 
             if (xCoord == pickedXCoord && yCoord > -1 && yCoord < 8 && (yCoord == 1 + xCoord || yCoord == 2 + xCoord)) {
-                gameState = "pendingWhite";
+                gameState = "pickBlue";
             }
 
         }
@@ -236,11 +238,11 @@ public class MultiplayerController {
     }
 
     @FXML
-    void whitePickSpot(int xCoord, int yCoord) {
+    void bluePickSpot(int xCoord, int yCoord) {
             String pieceName = chessBoard[pickedXCoord][pickedYCoord].chessPiece.name;
               if (pieceName.equals("bishop")) {
                   boolean noSpaceBetween = true;
-                if (((Math.abs(xCoord - pickedXCoord) == Math.abs(yCoord - pickedYCoord))) && (chessBoard[xCoord][yCoord].isEmpty || !chessBoard[xCoord][yCoord].chessPiece.isWhiteTeam)){
+                if (((Math.abs(xCoord - pickedXCoord) == Math.abs(yCoord - pickedYCoord))) && (chessBoard[xCoord][yCoord].isEmpty || !chessBoard[xCoord][yCoord].chessPiece.isBlueTeam)){
                     for(int xCheck = xCoord; xCheck < pickedXCoord; xCheck++) {
                         int yCheck = Math.abs(xCoord - pickedXCoord) + pickedYCoord;
                         if(!chessBoard[xCheck][yCheck].isEmpty) {
@@ -249,10 +251,10 @@ public class MultiplayerController {
                     }
                 }
                 if(noSpaceBetween) {
-                    gameState = "pendingBlack";
+                    gameState = "pickPurple";
                 }
             } else if (pieceName.equals("rook")) {
-                  if ((chessBoard[xCoord][yCoord].isEmpty || !chessBoard[xCoord][yCoord].chessPiece.isWhiteTeam)) {
+                  if ((chessBoard[xCoord][yCoord].isEmpty || !chessBoard[xCoord][yCoord].chessPiece.isBlueTeam)) {
                       boolean noSpaceBetween = true;
                       if(xCoord == pickedXCoord) {
                           for(int yCheck = yCoord; yCheck < pickedYCoord; yCheck++) {
@@ -269,11 +271,182 @@ public class MultiplayerController {
                       }
 
                       if(noSpaceBetween) {
-                          gameState = "pendingBlack";
+                          gameState = "pickPurple";
                       }
                   }
             }
         System.out.println(gameState);
+    }
+
+    public static Parent setRoot(ImageView imageView) throws IOException {
+        //purple rook
+        InputStream rookStream = new FileInputStream("src/sample/data/purple pieces/rook.png");
+        Image rook = new Image(rookStream);
+        ImageView rookView = new ImageView();
+        rookView.setImage(rook);
+        rookView.setMouseTransparent(true);
+        rookView.setX(-110);
+        rookView.setY(-40);
+
+        //purple knight
+        InputStream knightStream = new FileInputStream("src/sample/data/purple pieces/knight.png");
+        Image knight = new Image(knightStream);
+        ImageView knightView = new ImageView();
+        knightView.setImage(knight);
+        knightView.setMouseTransparent(true);
+        knightView.setX(-100);
+        knightView.setY(-40);
+
+        //purple bishop
+        InputStream bishopStream = new FileInputStream("src/sample/data/purple pieces/bishop.png");
+        Image bishop = new Image(bishopStream);
+        ImageView bishopView = new ImageView();
+        bishopView.setImage(bishop);
+        bishopView.setMouseTransparent(true);
+        bishopView.setX(-100);
+        bishopView.setY(-40);
+
+        //purple king
+        InputStream kingStream = new FileInputStream("src/sample/data/purple pieces/king.png");
+        Image king = new Image(kingStream);
+        ImageView kingView = new ImageView();
+        kingView.setImage(king);
+        kingView.setMouseTransparent(true);
+        kingView.setX(160);
+        kingView.setY(-40);
+
+        //purple queen
+        InputStream queenStream = new FileInputStream("src/sample/data/purple pieces/queen.png");
+        Image queen = new Image(queenStream);
+        ImageView queenView = new ImageView();
+        queenView.setImage(queen);
+        queenView.setMouseTransparent(true);
+        queenView.setX(290);
+        queenView.setY(-40);
+
+        //purple bishop2
+        ImageView bishop2View = new ImageView();
+        bishop2View.setImage(bishop);
+        bishop2View.setMouseTransparent(true);
+        bishop2View.setX(100);
+        bishop2View.setY(-40);
+
+        //purple knight2
+        ImageView knight2View = new ImageView();
+        knight2View.setImage(knight);
+        knight2View.setMouseTransparent(true);
+        knight2View.setX(230);
+        knight2View.setY(-40);
+
+        //purple rook2
+        ImageView rook2View = new ImageView();
+        rook2View.setImage(rook);
+        rook2View.setMouseTransparent(true);
+        rook2View.setX(360);
+        rook2View.setY(-40);
+
+        //purple pawn
+        InputStream pawnStream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
+        Image pawn = new Image(pawnStream);
+        ImageView pawnView = new ImageView();
+        pawnView.setImage(pawn);
+        pawnView.setMouseTransparent(true);
+        pawnView.setX(-100);
+        pawnView.setY(25);
+
+        //purple pawn2
+        InputStream pawn2Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
+        Image pawn2 = new Image(pawn2Stream);
+        ImageView pawn2View = new ImageView();
+        pawn2View.setImage(pawn2);
+        pawn2View.setMouseTransparent(true);
+        pawn2View.setX(-170);
+        pawn2View.setY(25);
+
+        //purple pawn3
+        InputStream pawn3Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
+        Image pawn3 = new Image(pawn3Stream);
+        ImageView pawn3View = new ImageView();
+        pawn3View.setImage(pawn3);
+        pawn3View.setMouseTransparent(true);
+        pawn3View.setX(-30);
+        pawn3View.setY(25);
+
+        //purple pawn4
+        InputStream pawn4Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
+        Image pawn4 = new Image(pawn4Stream);
+        ImageView pawn4View = new ImageView();
+        pawn4View.setImage(pawn4);
+        pawn4View.setMouseTransparent(true);
+        pawn4View.setX(40);
+        pawn4View.setY(25);
+
+        //purple pawn5
+        InputStream pawn5Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
+        Image pawn5 = new Image(pawn5Stream);
+        ImageView pawn5View = new ImageView();
+        pawn5View.setImage(pawn5);
+        pawn5View.setMouseTransparent(true);
+        pawn5View.setX(100);
+        pawn5View.setY(25);
+
+        //purple pawn6
+        InputStream pawn6Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
+        Image pawn6 = new Image(pawn6Stream);
+        ImageView pawn6View = new ImageView();
+        pawn6View.setImage(pawn6);
+        pawn6View.setMouseTransparent(true);
+        pawn6View.setX(170);
+        pawn6View.setY(25);
+
+        //purple pawn7
+        InputStream pawn7Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
+        Image pawn7 = new Image(pawn7Stream);
+        ImageView pawn7View = new ImageView();
+        pawn7View.setImage(pawn7);
+        pawn7View.setMouseTransparent(true);
+        pawn7View.setX(-300);
+        pawn7View.setY(25);
+
+        //purple pawn8
+        InputStream pawn8Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
+        Image pawn8 = new Image(pawn8Stream);
+        ImageView pawn8View = new ImageView();
+        pawn8View.setImage(pawn8);
+        pawn8View.setMouseTransparent(true);
+        pawn8View.setX(-240);
+        pawn8View.setY(25);
+
+        //blue rook
+        InputStream blueRookStream = new FileInputStream("src/sample/data/blue pieces/rook.png");
+        Image blueRook = new Image(blueRookStream);
+        ImageView blueRookView = new ImageView();
+        blueRookView.setImage(blueRook);
+        blueRookView.setMouseTransparent(true);
+        blueRookView.setX(-110);
+        blueRookView.setY(490);
+
+        //blue knight
+        InputStream blueKnightStream = new FileInputStream("src/sample/data/blue pieces/knight.png");
+        Image blueKnight = new Image(blueKnightStream);
+        ImageView blueKnightView = new ImageView();
+        blueKnightView.setImage(blueKnight);
+        blueKnightView.setMouseTransparent(true);
+        blueKnightView.setX(-110);
+        blueKnightView.setY(490);
+
+        //blue bishop
+        InputStream blueBishopStream = new FileInputStream("src/sample/data/blue pieces/bishop.png");
+        Image blueBishop = new Image(blueBishopStream);
+        ImageView blueBishopView = new ImageView();
+        blueBishopView.setImage(blueBishop);
+        blueBishopView.setMouseTransparent(true);
+        blueBishopView.setX(-110);
+        blueBishopView.setY(490);
+
+        return new Group(imageView,rookView,knightView,bishopView,kingView,queenView,bishop2View,knight2View,
+                rook2View,pawnView,pawn2View,pawn3View,pawn4View,pawn5View,pawn6View,pawn7View,pawn8View,
+                blueRookView,blueKnightView,blueBishopView);
     }
 
 }
