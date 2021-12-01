c92880b934a8f0b4a20f03d9f8da56388dc6f273
Franz Lleshaj <66387518+franzlleshaj@users.noreply.github.com>
2021-11-30 19:20:10 -0500

    fixed arrayCoordToPixel function

--- src/sample/MultiplayerController.java
+++ src/sample/MultiplayerController.java
@@ -118,9 +118,6 @@ public class MultiplayerController {
         stage.setScene(scene);
         stage.show();
 
-
-
-
         gameState = "pickBlue"; // blue piece needs to be picked
 
         startGame(event);
@@ -138,10 +135,10 @@ public class MultiplayerController {
             double boardXCoord = (xCoord - .0365) / (.965 - .0365); // relative coordinates to board
             double boardYCoord = (yCoord - .0365) / (.965 - .0365);
 
-            System.out.println(xCoord);
-            System.out.println(yCoord);
+            //System.out.println(xCoord);
+            //System.out.println(yCoord);
 
-            System.out.println(chessBoard[pickedXCoord][pickedYCoord].chessPiece.name);
+            //System.out.println(chessBoard[pickedXCoord][pickedYCoord].chessPiece.name);
             // if relative coordinates to board fall within 0% and 100%, they are converted to chess square
             // coordinates
             if (boardXCoord <= 1 && boardXCoord >= 0 && boardYCoord <= 1 && boardYCoord >= 0) {
@@ -443,6 +440,7 @@ public class MultiplayerController {
                 chessBoard[pickedXCoord][pickedYCoord].chessPiece.name = null;
                 chessBoard[pickedXCoord][pickedYCoord].isEmpty = true;
             }
+
         }
         System.out.println(gameState);
     }
@@ -677,7 +675,7 @@ public class MultiplayerController {
                       chessBoard[pickedXCoord][pickedYCoord].isEmpty = true;
                       chessBoard[pickedXCoord][pickedYCoord].chessPiece.isBlueTeam = false;
                   }
-              }
+            }
         System.out.println(gameState);
     }
 
@@ -728,13 +726,13 @@ public class MultiplayerController {
 
     public double[] arrayCoordToPixel(int xCoord, int yCoord) {
         double[] coordinates = new double[2];
-//      coordinates[0] = ((double) (xCoord / 8)) * imageView.getFitHeight();
-
-        coordinates[0] = ((((imageView.getFitHeight()*0.94)/8.0) * xCoord)+15);
-        coordinates[1] = ((((imageView.getFitHeight()*0.94)/8.0) * yCoord)+15);
+        coordinates[0] = ((((double) xCoord) / 8) * .93 + .03) * imageView.getFitWidth();
+        //System.out.println(coordinates[0]);
+        coordinates[1] = ((((double) yCoord) / 8) * .93 + .03) * imageView.getFitHeight();
         return coordinates;
     }
-    public static Parent setRoot(ImageView imageView) throws IOException {
+
+    public Parent setRoot(ImageView imageView) throws IOException {
         //purple rook
         InputStream rookStream = new FileInputStream("src/sample/data/purple pieces/rook.png");
         Image rook = new Image(rookStream);
@@ -743,10 +741,9 @@ public class MultiplayerController {
         //rookView.setFitWidth(imageView.getFitWidth());
         rookView.setImage(rook);
         rookView.setMouseTransparent(true);
-        rookView.setX(imageView.getX());
-        rookView.setY(imageView.getY());
-        rookView.setX(14);
-        rookView.setY(20);
+        double coordinates[] = arrayCoordToPixel(0, 0);
+        rookView.setX(coordinates[0]);
+        rookView.setY(coordinates[1]);
 
         //purple knight
         InputStream knightStream = new FileInputStream("src/sample/data/purple pieces/knight.png");
@@ -754,8 +751,9 @@ public class MultiplayerController {
         ImageView knightView = new ImageView();
         knightView.setImage(knight);
         knightView.setMouseTransparent(true);
-        knightView.setX(73);
-        knightView.setY(18);
+        coordinates = arrayCoordToPixel(1, 0);
+        knightView.setX(coordinates[0]);
+        knightView.setY(coordinates[1]);
 
         //purple bishop
         InputStream bishopStream = new FileInputStream("src/sample/data/purple pieces/bishop.png");
@@ -763,8 +761,9 @@ public class MultiplayerController {
         ImageView bishopView = new ImageView();
         bishopView.setImage(bishop);
         bishopView.setMouseTransparent(true);
-        bishopView.setX(146);
-        bishopView.setY(20);
+        coordinates = arrayCoordToPixel(2, 0);
+        bishopView.setX(coordinates[0]);
+        bishopView.setY(coordinates[1]);
 
         //purple king
         InputStream kingStream = new FileInputStream("src/sample/data/purple pieces/king.png");
@@ -772,8 +771,9 @@ public class MultiplayerController {
         ImageView kingView = new ImageView();
         kingView.setImage(king);
         kingView.setMouseTransparent(true);
-        kingView.setX(212);
-        kingView.setY(18);
+        coordinates = arrayCoordToPixel(3, 0);
+        kingView.setX(coordinates[0]);
+        kingView.setY(coordinates[1]);
 
         //purple queen
         InputStream queenStream = new FileInputStream("src/sample/data/purple pieces/queen.png");
@@ -781,29 +781,33 @@ public class MultiplayerController {
         ImageView queenView = new ImageView();
         queenView.setImage(queen);
         queenView.setMouseTransparent(true);
-        queenView.setX(289);
-        queenView.setY(21);
+        coordinates = arrayCoordToPixel(4, 0);
+        queenView.setX(coordinates[0]);
+        queenView.setY(coordinates[1]);
 
         //purple bishop2
         ImageView bishop2View = new ImageView();
         bishop2View.setImage(bishop);
         bishop2View.setMouseTransparent(true);
-        bishop2View.setX(345);
-        bishop2View.setY(20);
+        coordinates = arrayCoordToPixel(5, 0);
+        bishop2View.setX(coordinates[0]);
+        bishop2View.setY(coordinates[1]);
 
         //purple knight2
         ImageView knight2View = new ImageView();
         knight2View.setImage(knight);
         knight2View.setMouseTransparent(true);
-        knight2View.setX(406);
-        knight2View.setY(18);
+        coordinates = arrayCoordToPixel(6, 0);
+        knight2View.setX(coordinates[0]);
+        knight2View.setY(coordinates[1]);
 
         //purple rook2
         ImageView rook2View = new ImageView();
         rook2View.setImage(rook);
         rook2View.setMouseTransparent(true);
-        rook2View.setX(480);
-        rook2View.setY(20);
+        coordinates = arrayCoordToPixel(7, 0);
+        rook2View.setX(coordinates[0]);
+        rook2View.setY(coordinates[1]);
 
         //purple pawn
         InputStream pawnStream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
@@ -811,8 +815,9 @@ public class MultiplayerController {
         ImageView pawnView = new ImageView();
         pawnView.setImage(pawn);
         pawnView.setMouseTransparent(true);
-        pawnView.setX(18);
-        pawnView.setY(82);
+        coordinates = arrayCoordToPixel(0, 1);
+        pawnView.setX(coordinates[0]);
+        pawnView.setY(coordinates[1]);
 
         //purple pawn2
         InputStream pawn2Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
@@ -820,8 +825,9 @@ public class MultiplayerController {
         ImageView pawn2View = new ImageView();
         pawn2View.setImage(pawn2);
         pawn2View.setMouseTransparent(true);
-        pawn2View.setX(85);
-        pawn2View.setY(82);
+        coordinates = arrayCoordToPixel(1, 1);
+        pawn2View.setX(coordinates[0]);
+        pawn2View.setY(coordinates[1]);
 
         //purple pawn3
         InputStream pawn3Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
@@ -829,8 +835,9 @@ public class MultiplayerController {
         ImageView pawn3View = new ImageView();
         pawn3View.setImage(pawn3);
         pawn3View.setMouseTransparent(true);
-        pawn3View.setX(152);
-        pawn3View.setY(82);
+        coordinates = arrayCoordToPixel(2, 1);
+        pawn3View.setX(coordinates[0]);
+        pawn3View.setY(coordinates[1]);
 
         //purple pawn4
         InputStream pawn4Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
@@ -838,8 +845,9 @@ public class MultiplayerController {
         ImageView pawn4View = new ImageView();
         pawn4View.setImage(pawn4);
         pawn4View.setMouseTransparent(true);
-        pawn4View.setX(219);
-        pawn4View.setY(82);
+        coordinates = arrayCoordToPixel(3, 1);
+        pawn4View.setX(coordinates[0]);
+        pawn4View.setY(coordinates[1]);
 
         //purple pawn5
         InputStream pawn5Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
@@ -847,8 +855,9 @@ public class MultiplayerController {
         ImageView pawn5View = new ImageView();
         pawn5View.setImage(pawn5);
         pawn5View.setMouseTransparent(true);
-        pawn5View.setX(285);
-        pawn5View.setY(82);
+        coordinates = arrayCoordToPixel(4, 1);
+        pawn5View.setX(coordinates[0]);
+        pawn5View.setY(coordinates[1]);
 
         //purple pawn6
         InputStream pawn6Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
@@ -856,8 +865,9 @@ public class MultiplayerController {
         ImageView pawn6View = new ImageView();
         pawn6View.setImage(pawn6);
         pawn6View.setMouseTransparent(true);
-        pawn6View.setX(352);
-        pawn6View.setY(82);
+        coordinates = arrayCoordToPixel(5, 1);
+        pawn6View.setX(coordinates[0]);
+        pawn6View.setY(coordinates[1]);
 
         //purple pawn7
         InputStream pawn7Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
@@ -865,8 +875,9 @@ public class MultiplayerController {
         ImageView pawn7View = new ImageView();
         pawn7View.setImage(pawn7);
         pawn7View.setMouseTransparent(true);
-        pawn7View.setX(418);
-        pawn7View.setY(82);
+        coordinates = arrayCoordToPixel(6, 1);
+        pawn7View.setX(coordinates[0]);
+        pawn7View.setY(coordinates[1]);
 
         //purple pawn8
         InputStream pawn8Stream = new FileInputStream("src/sample/data/purple pieces/pawn.png");
@@ -874,8 +885,9 @@ public class MultiplayerController {
         ImageView pawn8View = new ImageView();
         pawn8View.setImage(pawn8);
         pawn8View.setMouseTransparent(true);
-        pawn8View.setX(485);
-        pawn8View.setY(82);
+        coordinates = arrayCoordToPixel(7, 1);
+        pawn8View.setX(coordinates[0]);
+        pawn8View.setY(coordinates[1]);
 
         //blue rook
         InputStream blueRookStream = new FileInputStream("src/sample/data/blue pieces/rook.png");
@@ -883,8 +895,9 @@ public class MultiplayerController {
         ImageView blueRookView = new ImageView();
         blueRookView.setImage(blueRook);
         blueRookView.setMouseTransparent(true);
-        blueRookView.setX(19);
-        blueRookView.setY(489);
+        coordinates = arrayCoordToPixel(0, 7);
+        blueRookView.setX(coordinates[0]);
+        blueRookView.setY(coordinates[1]);
 
         //blue knight
         InputStream blueKnightStream = new FileInputStream("src/sample/data/blue pieces/knight.png");
@@ -892,8 +905,9 @@ public class MultiplayerController {
         ImageView blueKnightView = new ImageView();
         blueKnightView.setImage(blueKnight);
         blueKnightView.setMouseTransparent(true);
-        blueKnightView.setX(80);
-        blueKnightView.setY(490);
+        coordinates = arrayCoordToPixel(1, 7);
+        blueKnightView.setX(coordinates[0]);
+        blueKnightView.setY(coordinates[1]);
 
         //blue bishop
         InputStream blueBishopStream = new FileInputStream("src/sample/data/blue pieces/bishop.png");
@@ -901,17 +915,19 @@ public class MultiplayerController {
         ImageView blueBishopView = new ImageView();
         blueBishopView.setImage(blueBishop);
         blueBishopView.setMouseTransparent(true);
-        blueBishopView.setX(148);
-        blueBishopView.setY(490);
+        coordinates = arrayCoordToPixel(2, 7);
+        blueBishopView.setX(coordinates[0]);
+        blueBishopView.setY(coordinates[1]);
 
-        //blue king
+        //blue queen
         InputStream blueKingStream = new FileInputStream("src/sample/data/blue pieces/king.png");
         Image blueKing = new Image(blueKingStream);
         ImageView blueKingView = new ImageView();
         blueKingView.setImage(blueKing);
         blueKingView.setMouseTransparent(true);
-        blueKingView.setX(213);
-        blueKingView.setY(490);
+        coordinates = arrayCoordToPixel(3, 7);
+        blueKingView.setX(coordinates[0]);
+        blueKingView.setY(coordinates[1]);
 
         //blue queen
         InputStream blueQueenStream = new FileInputStream("src/sample/data/blue pieces/queen.png");
@@ -919,8 +935,9 @@ public class MultiplayerController {
         ImageView blueQueenView = new ImageView();
         blueQueenView.setImage(blueQueen);
         blueQueenView.setMouseTransparent(true);
-        blueQueenView.setX(289);
-        blueQueenView.setY(490);
+        coordinates = arrayCoordToPixel(4, 7);
+        blueQueenView.setX(coordinates[0]);
+        blueQueenView.setY(coordinates[1]);
 
         //blue bishop2
         InputStream blueBishop2Stream = new FileInputStream("src/sample/data/blue pieces/bishop.png");
@@ -928,8 +945,9 @@ public class MultiplayerController {
         ImageView blueBishopView2 = new ImageView();
         blueBishopView2.setImage(blueBishop2);
         blueBishopView2.setMouseTransparent(true);
-        blueBishopView2.setX(348);
-        blueBishopView2.setY(490);
+        coordinates = arrayCoordToPixel(5, 7);
+        blueBishopView2.setX(coordinates[0]);
+        blueBishopView2.setY(coordinates[1]);
 
         //blue knight2
         InputStream blueKnight2Stream = new FileInputStream("src/sample/data/blue pieces/knight.png");
@@ -937,8 +955,9 @@ public class MultiplayerController {
         ImageView blueKnightView2 = new ImageView();
         blueKnightView2.setImage(blueKnight2);
         blueKnightView2.setMouseTransparent(true);
-        blueKnightView2.setX(414);
-        blueKnightView2.setY(490);
+        coordinates = arrayCoordToPixel(6, 7);
+        blueKnightView2.setX(coordinates[0]);
+        blueKnightView2.setY(coordinates[1]);
 
         //blue rook2
         InputStream blueRook2Stream = new FileInputStream("src/sample/data/blue pieces/rook.png");
@@ -946,8 +965,9 @@ public class MultiplayerController {
         ImageView blueRookView2 = new ImageView();
         blueRookView2.setImage(blueRook2);
         blueRookView2.setMouseTransparent(true);
-        blueRookView2.setX(488);
-        blueRookView2.setY(490);
+        coordinates = arrayCoordToPixel(7, 7);
+        blueRookView2.setX(coordinates[0]);
+        blueRookView2.setY(coordinates[1]);
 
         //blue pawn1
         InputStream bluePawnStream1 = new FileInputStream("src/sample/data/blue pieces/pawn.png");
@@ -955,8 +975,9 @@ public class MultiplayerController {
         ImageView bluePawnView1 = new ImageView();
         bluePawnView1.setImage(bluePawn1);
         bluePawnView1.setMouseTransparent(true);
-        bluePawnView1.setX(19);
-        bluePawnView1.setY(423);
+        coordinates = arrayCoordToPixel(0, 6);
+        bluePawnView1.setX(coordinates[0]);
+        bluePawnView1.setY(coordinates[1]);
 
         //blue pawn2
         InputStream bluePawnStream2 = new FileInputStream("src/sample/data/blue pieces/pawn.png");
@@ -964,8 +985,9 @@ public class MultiplayerController {
         ImageView bluePawnView2 = new ImageView();
         bluePawnView2.setImage(bluePawn2);
         bluePawnView2.setMouseTransparent(true);
-        bluePawnView2.setX(86);
-        bluePawnView2.setY(423);
+        coordinates = arrayCoordToPixel(1, 6);
+        bluePawnView2.setX(coordinates[0]);
+        bluePawnView2.setY(coordinates[1]);
 
         //blue pawn3
         InputStream bluePawnStream3 = new FileInputStream("src/sample/data/blue pieces/pawn.png");
@@ -973,8 +995,9 @@ public class MultiplayerController {
         ImageView bluePawnView3 = new ImageView();
         bluePawnView3.setImage(bluePawn3);
         bluePawnView3.setMouseTransparent(true);
-        bluePawnView3.setX(154);
-        bluePawnView3.setY(423);
+        coordinates = arrayCoordToPixel(2, 6);
+        bluePawnView3.setX(coordinates[0]);
+        bluePawnView3.setY(coordinates[1]);
 
         //blue pawn4
         InputStream bluePawnStream4 = new FileInputStream("src/sample/data/blue pieces/pawn.png");
@@ -982,8 +1005,9 @@ public class MultiplayerController {
         ImageView bluePawnView4 = new ImageView();
         bluePawnView4.setImage(bluePawn4);
         bluePawnView4.setMouseTransparent(true);
-        bluePawnView4.setX(219);
-        bluePawnView4.setY(423);
+        coordinates = arrayCoordToPixel(3, 6);
+        bluePawnView4.setX(coordinates[0]);
+        bluePawnView4.setY(coordinates[1]);
 
         //blue pawn5
         InputStream bluePawnStream5 = new FileInputStream("src/sample/data/blue pieces/pawn.png");
@@ -991,8 +1015,9 @@ public class MultiplayerController {
         ImageView bluePawnView5 = new ImageView();
         bluePawnView5.setImage(bluePawn5);
         bluePawnView5.setMouseTransparent(true);
-        bluePawnView5.setX(287);
-        bluePawnView5.setY(423);
+        coordinates = arrayCoordToPixel(4, 6);
+        bluePawnView5.setX(coordinates[0]);
+        bluePawnView5.setY(coordinates[1]);
 
         //blue pawn6
         InputStream bluePawnStream6 = new FileInputStream("src/sample/data/blue pieces/pawn.png");
@@ -1000,8 +1025,9 @@ public class MultiplayerController {
         ImageView bluePawnView6 = new ImageView();
         bluePawnView6.setImage(bluePawn6);
         bluePawnView6.setMouseTransparent(true);
-        bluePawnView6.setX(353);
-        bluePawnView6.setY(423);
+        coordinates = arrayCoordToPixel(5, 6);
+        bluePawnView6.setX(coordinates[0]);
+        bluePawnView6.setY(coordinates[1]);
 
         //blue pawn7
         InputStream bluePawnStream7 = new FileInputStream("src/sample/data/blue pieces/pawn.png");
@@ -1009,8 +1035,9 @@ public class MultiplayerController {
         ImageView bluePawnView7 = new ImageView();
         bluePawnView7.setImage(bluePawn7);
         bluePawnView7.setMouseTransparent(true);
-        bluePawnView7.setX(420);
-        bluePawnView7.setY(423);
+        coordinates = arrayCoordToPixel(6, 6);
+        bluePawnView7.setX(coordinates[0]);
+        bluePawnView7.setY(coordinates[1]);
 
         //blue pawn8
         InputStream bluePawnStream8 = new FileInputStream("src/sample/data/blue pieces/pawn.png");
@@ -1018,12 +1045,13 @@ public class MultiplayerController {
         ImageView bluePawnView8 = new ImageView();
         bluePawnView8.setImage(bluePawn8);
         bluePawnView8.setMouseTransparent(true);
-        bluePawnView8.setX(487);
-        bluePawnView8.setY(423);
+        coordinates = arrayCoordToPixel(7, 6);
+        bluePawnView8.setX(coordinates[0]);
+        bluePawnView8.setY(coordinates[1]);
 
         return new Group(imageView,rookView,knightView,bishopView,kingView,queenView,bishop2View,knight2View,
                 rook2View,pawnView,pawn2View,pawn3View,pawn4View,pawn5View,pawn6View,pawn7View,pawn8View,
-                blueRookView,blueKnightView,blueBishopView,blueKingView,blueQueenView,blueBishopView2,blueKnightView2,
+                blueRookView,blueKnightView,blueBishopView,blueKingView,blueBishopView2,blueKnightView2,
                 blueRookView2,bluePawnView1,bluePawnView2,bluePawnView3,bluePawnView4,bluePawnView5,bluePawnView6,bluePawnView7,bluePawnView8);
     }
 
